b70c0a61bfa7f4d37623852c81b45239f570b7b4
Jaeseob Hwang <jetsbee@gmail.com>
2019-05-13 17:53:13 +0900

    Added Editing text input

--- components/ToDo/index.js
+++ components/ToDo/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { Dimensions } from 'react-native';
 
 const { height, width } = Dimensions.get("window");
@@ -14,20 +14,32 @@ const ContainerView = styled.View`
   justify-content: space-between;
 `;
 
-const BasicText = styled.Text`
+const basicTextCSS = css`
   font-weight: 600;
   font-size: 20;
   margin-vertical: 20px;
   flex: 1;
 `;
 
-const CompletedText = styled(BasicText)`
+const BasicText = styled.Text`
+  ${basicTextCSS}
+`;
+
+const CompletedTextCSS = css`
   color: #bbb;
   text-decoration-line: line-through;
 `;
 
-const UncompletedText = styled(BasicText)`
+const CompletedText = styled(BasicText)`
+  ${CompletedTextCSS}
+`;
+
+const UncompletedTextCSS = css`
   color: #353839;
+`;
+
+const UncompletedText = styled(BasicText)`
+  ${UncompletedTextCSS}
 `;
 
 const TouchableOpacity = styled.TouchableOpacity``;
@@ -66,17 +78,46 @@ const ActionText = styled.Text`
   font-size: 15px;
 `;
 
+const EditingTextInput = styled.TextInput`
+  ${basicTextCSS}
+  width: ${width / 1.5};
+  margin-vertical: 15px;
+  padding-bottom: 5px;
+  ${
+    props => props.completedStyle
+    ? CompletedTextCSS
+    : UncompletedTextCSS
+  }
+`;
+
 export default class ToDo extends Component {
   static propTypes = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isEditing: true
+    };
+  }
+
   render() {
     const { id, text, isCompleted, uncomplete, complete } = this.props;
+    const { isEditing } = this.state;
     return (
       <ContainerView>
         <ColumnView>
           <TouchableOpacity onPressOut={() => (isCompleted ? uncomplete(id) : complete(id))}>
             { isCompleted ? <CompleteRadioView /> : <UncompleteRadioView /> }
           </TouchableOpacity>
-          { isCompleted ? <CompletedText>{text}</CompletedText> : <UncompletedText>{text}</UncompletedText> }
+          { isEditing ?
+            <EditingTextInput
+              multiline={true}
+              onChangeText={(text)=>{console.log(text)}}
+              onEndEditing={()=>{console.log('END EDITING!!')}}
+              value={text}
+              completedStyle={isCompleted}
+            /> :
+            isCompleted ? <CompletedText>{text}</CompletedText> : <UncompletedText>{text}</UncompletedText> }
         </ColumnView>
         <ActionsView>
           <TouchableOpacity>
